649bc4b0feec1d2cc1b4b089a706838f20e4f5f5
duskstone <jedgardromero@gmail.com>
2020-05-20 13:23:24 -0400

    added Game Component

--- components/game.jsx
+++ components/game.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+// import Board from './board.jsx'
+// import Minesweeper from '../minesweeper.js'
+// import * as Minesweeper from '../minesweeper.js'
+// import Board from "../minesweeper.js"
+import * as Minesweeper from "../minesweeper";
+// import Board from './board.jsx'
 
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        const board = new Minesweeper.Board(9, 20);
+        this.state = {
+            board: board
+        }
+        // const board = 
+       this.updateGame = this.updateGame.bind(this);
+    }
+
+    updateGame(){
+
+    }
+    // ghost tabs?
     render() {
-        return (
-            <div>This is the game! :)</div>
-        );
+        return <Board board={this.state.board} updateGame={this.updateGame} />;
+        // return <div>"This is game :3"</div>
     }
 };
 
