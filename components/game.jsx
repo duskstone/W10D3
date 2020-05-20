c168c8f0bde0c4eb088c0415e5b746dcb8c91b23
duskstone <jedgardromero@gmail.com>
2020-05-20 11:09:19 -0400

    Initial Commit

---
+++ components/game.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div>This is the game! :)</div>
+        );
+    }
+};
+
+export default Game;
